8a349714c883950c1995e63bc3a58395e23dcf97
T1M0N2 <131168002+T1M0N2@users.noreply.github.com>
2024-04-16 10:57:53 +0200

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -16,19 +16,18 @@ function showInstallPrompt() {
     // Hier können Sie Ihren eigenen Code hinzufügen, um das Installations-Popup anzuzeigen
 }
 
-// Der restliche Code Ihres Service Worker-Skripts bleibt unverändert
+// Aktualisierter Code für die Installation und Abfrage von Ressourcen aus dem Cache im Offline-Modus
 const cacheName = 'pwa-cache-v1';
 const filesToCache = [
     '/',
     'index.html',
     'manifest.json',
     'home.png',
-	'style.css',
-	'script.js',
-
-    // Fügen Sie hier weitere Ressourcen hinzu, die gecacht werden sollen
+    'style.css',
+    'script.js',
+    // Weitere Ressourcen, die gecacht werden sollen, hinzufügen
 ];
- 
+
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName)
@@ -37,12 +36,29 @@ self.addEventListener('install', event => {
             })
     );
 });
- 
+
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                return response || fetch(event.request);
+                // Wenn die Ressource im Cache gefunden wird, geben Sie sie zurück
+                if (response) {
+                    return response;
+                }
+                // Andernfalls versuchen Sie, die Ressource vom Netzwerk abzurufen
+                return fetch(event.request)
+                    .then(response => {
+                        // Wenn die Ressource erfolgreich vom Netzwerk abgerufen wurde, fügen Sie sie dem Cache hinzu
+                        return caches.open(cacheName)
+                            .then(cache => {
+                                cache.put(event.request.url, response.clone());
+                                return response;
+                            });
+                    })
+                    .catch(() => {
+                        // Wenn der Abruf fehlschlägt und die Ressource nicht im Cache vorhanden ist, geben Sie eine Offline-Seite zurück
+                        return caches.match('/offline.html');
+                    });
             })
     );
 });
