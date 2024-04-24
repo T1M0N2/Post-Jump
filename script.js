f0001ac181628e9d307093033c3c34ef1e91b311
T1M0N2 <131168002+T1M0N2@users.noreply.github.com>
2024-04-24 09:09:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,85 +1,140 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Post Jump</title>
-  <link rel="icon" href="home.png">
-  <link rel="manifest" href="manifest.json">
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <script>
-    // Variables
-    const leaderboardSize = 3; // Größe der Bestenliste
-    let leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
+// Variables
+let isJumping = false;
+let position = 55;
+let obstaclePosition = 600;
+let score = 0;
+let gameInterval;
+let speedIncreaseInterval;
+const jumpSpeed = 10;
+const topSpeed = 12; // Maximalgeschwindigkeit für Hindernisse
+let obstacleSpeed = 5;
+const leaderboardSize = 5; // Größe der Bestenliste
+let leaderboard = [];
 
-    // Functions
-    function updateLeaderboard() {
-      let leaderboardList = document.getElementById("leaderboard-list");
-      leaderboardList.innerHTML = ""; // Clear previous entries
-      leaderboard.forEach((score, index) => {
-        let listItem = document.createElement("li");
-        listItem.textContent = "Run " + (index + 1) + ": " + score;
-        leaderboardList.appendChild(listItem);
-      });
+// Functions
+function jump() {
+  let mario = document.getElementById("mario");
+  if (!isJumping) {
+    isJumping = true;
+    let upInterval = setInterval(() => {
+      if (position >= 207) {
+        clearInterval(upInterval);
+        let downInterval = setInterval(() => {
+          if (position <= 57) {
+            clearInterval(downInterval);
+            isJumping = false;
+          } else {
+            position -= jumpSpeed;
+            mario.style.bottom = position + "px";
+          }
+        }, 29);
+      } else {
+        position += jumpSpeed;
+        mario.style.bottom = position + "px";
+      }
+    }, 25);  
+  }
+}
+
+function update() {
+  let mario = document.getElementById("mario");
+  let obstacle = document.getElementById("obstacle");
+  let obstacles = [obstacle];
+  
+  if (obstaclePosition < 585) {
+    obstaclePosition += obstacleSpeed;
+    obstacle.style.right = obstaclePosition + "px";
+  } else {
+    obstaclePosition = -35;
+    score += 1;
+    document.getElementById("score").innerHTML = "Score: " + score;
+  }
+  
+  if (score % 200 === 0 && score > 0 && obstacleSpeed < topSpeed) {
+    clearInterval(speedIncreaseInterval);
+    obstacleSpeed += 1;
+    if (obstacleSpeed >= topSpeed) {
+      obstacleSpeed = topSpeed;
     }
+    speedIncreaseInterval = setInterval(() => {
+      if (obstacleSpeed < topSpeed) {
+        obstacleSpeed += 1;
+      }
+      if (obstacleSpeed >= topSpeed) {
+        clearInterval(speedIncreaseInterval);
+      }
+    }, 23000);
+  }
 
-    function gameOver() {
-      clearInterval(gameInterval); // stop the game loop
-      let endScreen = document.getElementById("game-over");
-      endScreen.style.display = "block"; // show the game over screen
-
-      // Save score to leaderboard
-      leaderboard.push(score);
-      leaderboard.sort((a, b) => b - a); // Sort in descending order
-      leaderboard = leaderboard.slice(0, leaderboardSize); // Keep only the top scores
-      localStorage.setItem('leaderboard', JSON.stringify(leaderboard)); // Save to local storage
-
-      // Display leaderboard
-      updateLeaderboard();
-
-      let restartButton = document.getElementById("restart-button");
-      restartButton.addEventListener("click", () => {
-        location.reload(); // refresh the page to restart the game
-      });
+  for (let i = 0; i < obstacles.length; i++) {
+    let obstacleWidth = parseInt(window.getComputedStyle(obstacles[i]).getPropertyValue('width'));
+    let marioHeight = parseInt(window.getComputedStyle(mario).getPropertyValue('height'));
+    let marioWidth = parseInt(window.getComputedStyle(mario).getPropertyValue('width'));
+    let marioBottom = position + marioHeight;
+    let obstacleLeft = parseInt(window.getComputedStyle(obstacles[i]).getPropertyValue('left'));
+    let obstacleBottom = parseInt(window.getComputedStyle(obstacles[i]).getPropertyValue('bottom'));
+    
+    if (
+      (obstacleLeft < marioWidth && obstacleLeft > 0) 
+      && (marioBottom >= obstacleBottom)
+      && (obstacleBottom + obstacleWidth >= position)
+    ) {
+      gameOver();
     }
+  }
 
-    // Main
-    document.getElementById("start-button").addEventListener("click", () => {
-      startGame();
-    });
+  score += 1;
+  document.getElementById("score").innerHTML = "Score: " + score;
 
-    // Initialize leaderboard on page load
-    updateLeaderboard();
-  </script>
-</head>
-<body>
-  <center><img src="titel.png" alt="Mein Spiel Logo"></center> 
-  <div id="game">
-    <div id="score">Score: 0</div>
-    <div id="mario"></div>
-    <div id="obstacle"></div>
-    <button id="start-button">Start</button>
-    <div id="game-over">
-      <img src="gameover1.png" alt="Game Over">
-      <p id="score-text"></p>
-      <button id="restart-button"></button>
-    </div>
-  </div>
-  <div id="leaderboard">
-    <h2>Bestenliste</h2>
-    <ol id="leaderboard-list"></ol>
-  </div>
-  <script src="script.js"></script>
-  <script>
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/serviceworker.js')
-          .then(registration => {
-            console.log('Service Worker registered: ', registration);
-          })
-          .catch(error => {
-            console.log('Service Worker registration failed: ', error);
-          });
-      });
+  mario.style.bottom = position + "px";
+}
+
+function startGame() {
+  document.getElementById("start-button").style.display = "none";
+  document.getElementById("game").setAttribute("tabindex", "0");
+  document.getElementById("game").focus();
+  gameInterval = setInterval(() => { // update the global variable instead of creating a new one
+    update();
+  }, 20);
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Space") {
+      jump();
     }
-  </script>
-</body>
-</html>
+  });
+
+  // Added event listener for touchstart event
+  document.addEventListener("touchstart", () => {
+    jump();
+  });
+}
+
+function gameOver() {
+  clearInterval(gameInterval); // stop the game loop
+  let endScreen = document.getElementById("game-over");
+  endScreen.style.display = "block"; // show the game over screen
+  
+  // Save score to leaderboard
+  leaderboard.push(score);
+  leaderboard.sort((a, b) => b - a); // Sort in descending order
+  leaderboard = leaderboard.slice(0, leaderboardSize); // Keep only the top scores
+
+  // Display leaderboard
+  let leaderboardList = document.getElementById("leaderboard-list");
+  leaderboardList.innerHTML = ""; // Clear previous entries
+  leaderboard.forEach((score, index) => {
+    let listItem = document.createElement("li");
+    listItem.textContent = "Run " + (index + 1) + ": " + score;
+    leaderboardList.appendChild(listItem);
+  });
+
+  let restartButton = document.getElementById("restart-button");
+  restartButton.addEventListener("click", () => {
+    location.reload(); // refresh the page to restart the game
+  });
+}
+
+// Main
+document.getElementById("start-button").addEventListener("click", () => {
+  startGame();
+});
